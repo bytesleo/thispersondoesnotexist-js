2bd00eb5b798273a45a72648cab101ac0031e1ad
Leonardo Rico <leonardo.ricog@gmail.com>
2019-02-24 01:21:00 -0400

    Updated

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ const dnte = new ThisPersonDoesNotExist();
 
 dnte
 	.getImage({
-		width: 256, // Width (default 128)
-		height: 256, // Height (default 128)
-		type: 'file' // file, Base64 (default file)
-		//path: 'avatars' // (default ./)
+		width: 256,
+		height: 256,
+		type: 'file'
 	})
 	.then(res => {
 		console.log('result->', res);
 	})
 	.catch(err => {
 		console.log('error->', err);
-	})
-
-// dnte
-// 	.on('created', (info) => {
-// 		console.log('cron->', info);
-// 	}).cron({
-// 		time: '*/10 * * * * *', // 10 seconds
-// 	});
+	});
